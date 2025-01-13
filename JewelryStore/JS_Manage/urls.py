2db8be26eed4ad8746c8ef6dfa165f80fd71ad86
MinhQuang <huynhminhquang2206@gmail.com>
2025-01-13 19:54:57 +0700

    test sql

--- JewelryStore/JS_Manage/urls.py
+++ JewelryStore/JS_Manage/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('JS_Manage/', views.hoaDon, name='Hoa don'),
-    path('JS_Manage/', views.nhanVien, name='Nhan vien'),
+    path('JS_Manage/', views.hoaDon, name='Bill'),
+    path('JS_Manage/', views.nhanVien, name='Staff'),
 ]
--- JewelryStore/JewelryStore/urls.py
+++ JewelryStore/JewelryStore/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
 
     path('', include('home.urls')),
+    path('', include('JS_Manage.urls')),
     path('admin/', admin.site.urls),
 ]
