6568efc66738d1207f4d14f00ac5111cf32c4e4a
ThanhhLichh <buithanhlich931@gmail.com>
2025-01-15 22:20:58 +0700

    Update templates

--- JewelryStore/home/urls.py
+++ JewelryStore/home/urls.py
@@ -5,6 +5,10 @@ from .import views
 urlpatterns = [
     
     path("", views.home, name= "home"),
-    path('login/', views.loginUser, name= "login"),
+    path("index.html", views.index, name= "index"),
+    path("index2.html", views.index2, name= "index2"),
+    path("index3.html", views.index3, name= "index3"),
+    path("widgets/cards.html", views.cards, name="cards"),
+    path("admin/", admin.site.urls),
     
 ]
--- JewelryStore/home/views.py
+++ JewelryStore/home/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request, 'home/home.html')
-def loginUser(request):
-    return render(request, 'home/login.html')
-
+def index (request):
+    return render(request, 'home/pages/index.html')
+def index2 (request):
+    return render(request, 'home/pages/index2.html')
+def index3 (request):
+    return render(request, 'home/pages/index3.html')
+def cards(request):
+    return render(request, 'home/pages/widgets/cards.html')
