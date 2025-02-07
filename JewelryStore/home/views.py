75876586a837069037c0e617e62a2e77f4284b4d
ThanhhLichh <buithanhlich931@gmail.com>
2025-02-07 11:39:57 +0700

    fix

--- JewelryStore/home/views.py
+++ JewelryStore/home/views.py
@@ -28,7 +28,7 @@ def nhanVien_list(request):
     return render(request, 'home/pages/index2.html', {'nhanViens': nhanViens})  # Truyền đúng biến vào template
 def bangGiaVang_list(request):
     bangGiaVangs = bangGiaVang.objects.all()
-    return render(request, 'home/pages/gold/gold-price.html', {'bangGiaVangs': bangGiaVangs})  # Truyền đúng biến vào template
+    return render(request, 'home/gold/gold-price.html', {'bangGiaVangs': bangGiaVangs})  # Truyền đúng biến vào template
 def sanPham_list(request):
     sanPhams = sanPham.objects.all()
     return render(request, 'home/products/product.html', {'sanPhams': sanPhams})
